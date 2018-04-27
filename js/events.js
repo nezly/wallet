fd6de3a7f49672d824d2fe9d93ac0c9137ad8dca
diego-z <irvtechnology@gmail.com>
2018-04-27 00:34:59 -0700

    added balances, help and copy address to clipboard for xlm and nez

--- js/events.js
+++ js/events.js
@@ -14,204 +14,29 @@ $(function() {
         },
         rules:
         {
-            email:
+            secretkey:
             {
                 required: true,
-                email: true
-            },
-            password:
-            {
-                required: true
+                secretStartsWithS: true,
+                minlength: 56,
+                maxlength: 56,
             }
         }
     });
 
-
-    $("#registration-form").validate(
-    {
-        submitHandler: function (form)
-        {
-            //check_captcha();
-            register();
-        },
-        rules:
-        {
-            firstname:
-            {
-                required: true
-            },
-            lastname:
-            {
-                required: true
-            },
-            email:
-            {
-                required: true,
-                email: true
-            },
-            password:
-            {
-                required: true,
-                minlength: 6
-            },
-            confirmpassword:
-            {
-                required: true,
-                minlength: 6,
-                equalTo: "#password"
-            }
-        }
-    });
 
     $(document).on('click', '.new', function()
     {
         page_new_wallet();
     });
 
-    $(document).on('click', '#buy-with-btc', function()
+    $(document).on('click', '.publickey', function()
     {
-        page_buy_with_btc();
+        sweet_popup("Your Public Address", 'Your Public Address can be shared with others to receive payments of both Stellar Lumens (XLM) and Nezly Tokens (NEZ) from other wallets or exchanges.', "info");
     });
 
-    $(document).on('click', '#buy-with-eth', function()
+    $(document).on('click', '.copyaddress', function()
     {
-        page_buy_with_eth();
+        copy_public_address();
     });
-
-    $(document).on('click', '#buy-with-xlm', function()
-    {
-        page_buy_with_xlm();
-    });
-
-    $(document).on('click', '#buy-with-usd', function()
-    {
-        page_buy_with_usd();
-    });
-
-    $(document).on('click', '#btcbuy_cancel', function()
-    {
-        page_buy();
-    });
-
-    $(document).on('click', '#ethbuy_cancel', function()
-    {
-        page_buy();
-    });
-
-    $(document).on('click', '#xlmbuy_cancel', function()
-    {
-        page_buy();
-    });
-
-    $(document).on('click', '#new-transaction', function()
-    {
-        page_buy();
-    });
-
-    $(document).on('click', '#usdbuy_cancel', function()
-    {
-        page_buy();
-    });
-
-    $(document).on('click', '#account_cancel', function()
-    {
-        load_user(page_buy);
-    });
-
-    $(document).on('click', '.transactions', function()
-    {
-        page_transactions();
-    });
-
-    $(document).on('click', '#sent-payment', function()
-    {
-        payment_sent();
-    });
-
-    $(document).on('click', '.nezwrapper', function()
-    {
-        confirmed_nez_popup();
-    });
-
-    $(document).on('click', '.account', function()
-    {
-        page_account();
-    });
-
-    $(document).on('click', '.bounty', function()
-    {
-        page_bounty();
-    });
-
-    $(document).on('click', '.settings', function()
-    {
-        page_settings();
-    });
-
-    $(document).on('click', '.button-enable-2fa', function(event)
-    {
-        event.preventDefault();
-        activate_2fa();
-    });
-
-    $(document).on('click', '.button-disable-2fa', function(event)
-    {
-        disable_2fa();
-    });
-
-    $(document).on('click', '.xlm-cancel-transaction', function()
-    {
-        cancel_transaction();
-    });
-
-    $(document).on('click', '.btc-cancel-transaction', function()
-    {
-        cancel_transaction();
-    });
-
-    $(document).on('click', '.eth-cancel-transaction', function()
-    {
-        cancel_transaction();
-    });
-
-    $(document).on('click', '.copy-referral-bounty-link', function()
-    {
-        copy_bounty_referral_link();
-    });
-
-    $(document).on('click', '.txn-click', function(event)
-    {
-        event.preventDefault();
-
-        var url = $(this).attr('data-url');
-
-        if (url === 'pending')
-        {
-            //sweet_popup("Pending", "This transaction is currently pending. Once manually verified, a link will be provided to view the transaction with a block explorer link.", "info");
-            show_pending_transaction();
-        }
-        else if (url === 'cancelled')
-        {
-            sweet_popup("Cancelled Transaction", "This transaction has been cancelled but it remains here for your records.", "info");
-        }
-        else if (url.substring(0, 5) === 'popup')
-        {
-            sweet_popup("Free Nezly Tokens", url.substring(5), "success");
-        }
-        else
-        {
-            window.open(url);
-        }
-    });
-
-
-
-
-    /*
-        $(document).on('click', '#bifrost-info', function()
-        {
-            sweet_popup('BiFrost Info', "Bifrost is highly available and secure Bitcoin/Ethereum → Stellar bridge developed by Stellar. It allows users to move BTC/ETH to the Stellar network and automatically trade them for Nezly Tokens at current rates shown on this page.",'info');
-        });
-    */
-
 });
