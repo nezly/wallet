22e4d77f4af7d20893ef65a4deae5ffa1762a313
diego-z <irvtechnology@gmail.com>
2018-04-30 23:51:02 -0700

    added transaction form

--- templates/wallet.vue
+++ templates/wallet.vue
@@ -22,21 +22,35 @@
     <div class="wrapper wrapper-content no-margins no-padding">
 
         <div class="inner_content no-margins">
-            <div class="main_container">
+            <div style="margin-top: 16px;">
                 <div class="checkmail">
-                    <div class="contact_form no-margins">
-                        <h3 class="nezly-blue" style="margin-top: 0px;">Public Address:</h3>
-                        <p id="public_key" style="text-align: left;">{{ public_key }}<br /><span class="pull-left"><a href="#" class="copyaddress">Copy</a></span><span class="pull-right"><a href="#" class="publickey">What's this?</a></span></p>
-                        <hr style="width: 100%; border-top: 1px solid #00aeef;" />
-                        <h3 class="nezly-blue" style="margin-top: 0px;">Balance:</h3>
-                        <h2 id="xlm_line" class="nezly-blue font-bold" style="margin-top: 0px;">XLM: <span class="text-white" id="xlmtotal" v-cloak="true"></span></h2>
-                        <h2 id="nez_line" class="nezly-blue font-bold" style="margin-top: 0px;">NEZ: <span class="text-white" id="neztotal" v-cloak="true"></span></h2>
-                        <hr style="width: 100%; border-top: 1px solid #00aeef;" />
-                        <form id="payment-form" method="post" action="#" style="display: none;">
-                            <div class="form-group">
-                                <div class="all_fields"><input type="submit" value="PREVIEW PAYMENT" /></div>
+                    <div class="contact_form no-margins" style="width:100%;">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <h3 class="nezly-blue" style="margin-top: 0px;">Your Public Address:</h3>
+                                <p id="public_key" style="text-align: left;">{{ public_key }}<br /><span class="pull-left"><a href="#" class="copyaddress">Copy</a></span><span class="pull-right"><a href="#" class="publickey">What's this?</a></span></p>
+                                <hr style="width: 100%; border-top: 1px solid #00aeef;" />
                             </div>
-                        </form>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-4">
+                                <h3 class="nezly-blue" style="margin-top: 0px;">Your Balance:</h3>
+                                <h2 id="xlm_line" class="nezly-blue font-bold" style="margin-top: 0px;">XLM: <span class="text-white" id="xlmtotal" v-cloak="true"></span></h2>
+                                <h2 id="nez_line" class="nezly-blue font-bold" style="margin-top: 0px;">NEZ: <span class="text-white" id="neztotal" v-cloak="true"></span></h2>
+                            </div>
+                            <div class="col-md-8">
+                                <h3 class="nezly-blue" style="margin-top: 0px;">Transaction:</h3>
+                                <form id="payment-form" method="post" action="#">
+                                    <div class="form-group" style="margin: 0;">
+                                        <input id="toaddress" name="toaddress" type="text" placeholder="Recipient Address" class="all_field" style="margin-top: 0px !important;" />
+                                        <input id="amount" name="amount" type="text" placeholder="Amount" class="all_field" style="display: inline; width: 50%; margin-right: 16px;" />
+                                        <input id="amount2" name="amount2" type="text" placeholder="Amount" class="all_field" style="width: 40%;" />
+                                        <input id="memotext" name="memotext" type="text" placeholder="Message (Optional)" class="all_field" />
+                                        <div class="all_fields"><input type="submit" value="PREVIEW PAYMENT" /></div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
